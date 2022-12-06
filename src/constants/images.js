f6ff34ad7c2cd2ac2bedae3771c16aafbb6e55c6
loaymoolb <loya130513@gmail.com>
2022-12-06 22:58:44 +0100

    с2
    added files

---
+++ src/constants/images.js
@@ -0,0 +1,40 @@
+import bootstrap from '../assets/bootstrap.png';
+import csharp from '../assets/csharp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import nextjs from '../assets/nextjs.png';
+import node from '../assets/node.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sanity from '../assets/sanity.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import tailwind from '../assets/tailwind.png';
+
+import random from '../assets/random.jpg';
+import profile from '../assets/profile.jpg';
+import logo from '../assets/logo.svg';
+
+export default {
+  bootstrap,
+  csharp,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nextjs,
+  node,
+  react,
+  redux,
+  sanity,
+  sass,
+  tailwind,
+  typescript,
+  random,
+  profile,
+  logo
+};
